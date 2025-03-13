792550c7ec816fdcfaaa26421f87efb36de465c6
GizemKayaa <giizeeem61@gmail.com>
2025-03-13 17:34:25 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
+// Database configuration
+$db_host = 'appmod-phpapp';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
-
+$db_user = 'appmod-phpapp-user';
+$db_pass = 'burcugizem9600';
+ 
 try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Errore di connessione: " . $e->getMessage());
+    die("Errore di connessione: " . $e->getMessage());
 }
-
+ 
 session_start();
 ?>
